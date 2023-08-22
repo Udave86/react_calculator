5f9bf936878977a49409c4aad81429cdf304efe5
Uday Dave <udave86@gmail.com>
2023-08-22 16:27:11 +0530

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import moonIcon from './assets/moon.png';
+import sunIcon from './assets/sun.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app_calculator">
+        <div className="app_calculator_navbar">
+          <div className="app_calculator_navbar_toggle">
+            <div className="app_calculator_navbar_toggle_circle">
+
+            </div>
+          </div>
+          <img src="" alt="mode"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
